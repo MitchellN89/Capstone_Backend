e311d50e104a84c65afbbb4e5f125074b7555723
MitchellN89 <mitchell.nilsson@gmail.com>
2023-12-23 17:56:12 +1300

    Setting up boiler plate project

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send({ result: "You've made it to the root route" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
