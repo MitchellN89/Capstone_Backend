09ebbd2eae23512d084515c0891f25b82489b355
MitchellN89 <mitchell.nilsson@gmail.com>
2023-12-27 14:29:42 +1300

    Created index.js for routes to help cleanup server page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { authRoutes } = require("./routes");
 
 require("dotenv").config();
 const dbConnect = require("./dbConnect");
@@ -8,14 +9,13 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 
+app.use("/auth", authRoutes);
+
 const { tokenChecker } = require("./middleware");
 app.post("/token", tokenChecker, (req, res) => {
   const body = req.body;
   res.send({ response: "Token must have been ok!", body: body });
 });
-
-const authRoutes = require("./routes/authRoutes");
-app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
